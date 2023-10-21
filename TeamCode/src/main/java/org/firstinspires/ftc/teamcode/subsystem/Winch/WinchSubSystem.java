2349202451cfc7f10757a58f85240f34eb16e20b
FTC18228 <18228@buildingblockstudio.com>
2023-10-21 16:16:26 +1000

    WinchSubSystem implemented

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/Winch/WinchSubSystem.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.subsystem.Winch;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+
+public class WinchSubSystem extends SubsystemBase {
+
+
+    public WinchSubSystem(){
+
+    }
+}
